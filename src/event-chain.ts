0897850fee721ab8123c41b8a981235ce42314e9
PragmaticBeaver <pragmaticbeaver@gmx.de>
2020-11-24 20:53:39 +0100

    [event-chain] del: types.d.ts
    - types were moved into event-chain.ts

--- src/event-chain.ts
+++ src/event-chain.ts
@@ -1,4 +1,16 @@
-import { ChainCallback, ChainToken, IChain } from "./event-chain-types";
+export interface IChain {
+  add(placeInOrder: number, cb: ChainCallback): ChainToken;
+  remove(token: ChainToken): void;
+  call(...args: unknown[]): Promise<unknown>;
+  isEmpty(): boolean;
+}
+
+export interface ChainToken {
+  id: number;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ChainCallback = (...args: any[]) => Promise<any>;
 
 class ChainLink {
   private callback: ChainCallback;
