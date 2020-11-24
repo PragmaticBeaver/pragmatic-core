df6f154cd8ce22488c66d9557e310c3d3e0abcbd
PragmaticBeaver <pragmaticbeaver@gmx.de>
2020-11-24 16:30:51 +0100

    [main] mod: changed exports

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { EventBus, EventToken } from "./event-bus";
-import { IPlugin } from "./plugin";
+import * as EventBus from "./event-bus";
+import * as Plugin from "./plugin";
+import * as PluginConfig from "./plugin-config";
+import * as EventChainTypes from "./event-chain-types";
+import * as EventChain from "./event-chain";
+import * as PluginManager from "./plugin-manager";
 
-export { EventBus, EventToken, IPlugin };
+export { EventBus, Plugin, PluginConfig, EventChainTypes, EventChain, PluginManager};
